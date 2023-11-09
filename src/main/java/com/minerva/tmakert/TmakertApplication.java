d0cf955e89ffa2c0e04e6c1715dff827eab83079
kainbr <kainbr@naver.com>
2023-11-09 14:38:42 +0900

    [create] 새 프로젝트 생성

---
+++ src/main/java/com/minerva/tmakert/TmakertApplication.java
@@ -0,0 +1,13 @@
+package com.minerva.tmakert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmakertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmakertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minerva/tmakert/TmakertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minerva.tmakert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmakertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
